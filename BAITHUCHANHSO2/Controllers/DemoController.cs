508e0a5ac9907fc5db1df10d58bc786dae694aaf
Admin <Admin@LAPTOP-2LGM8HPC>
2021-08-31 14:19:47 +0700

    123

---
+++ BAITHUCHANHSO2/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Baitap2.models;
+
+namespace BAITHUCHANHSO2.Controllers
+{
+    public class DemoController : Controller
+    {
+        Giaiphuongtrinh Gtp = new Giaiphuongtrinh()
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Tinhtong(string soA, string soB)
+        {
+            int Sothunhat = Convert.ToInt32(soA);
+            int Sothuhai = Convert.ToInt32(SoB);
+            int Tonghaiso = Gtp.TinhTong(Sothunhat, Sothuhai);
+            ViewBag.Tong = Tonghaiso;
+            return View()
+        }
+     }
+}
--- BAITHUCHANHSO2/Models/Giaiphuongtrinh.cs
+++ BAITHUCHANHSO2/Models/Giaiphuongtrinh.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//using cac thu vien can thiet
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,12 @@ namespace BAITHUCHANHSO2.Models
 {
     public class Giaiphuongtrinh
     {
+        public int Tinhtong(int a, int b)
+        {
+            int tong = 0;
+            tong = a + b;
+            return tong;
+        }
+
     }
 }
